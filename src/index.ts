4145190cd4b7726e0eefb2531c53e730b7078dcb
Chao Ma <machaosydney@gmail.com>
2023-07-12 15:31:20 +1000

    feat(users): Add user model and CRUD operations

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
+require('dotenv').config();
 
 const app = express();
 
@@ -18,6 +20,14 @@ app.use(compression());
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-	console.log('server is running on http://localhost:8080/');
+server.listen(8090, () => {
+	console.log('server is running on http://localhost:8090/');
 });
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGO_URI);
+mongoose.connection.on('error', (err) =>
+	console.error('MongoDB connection error: ' + err)
+);
+
+
