5b2a241b3eb817a51149bb64af0335895a2e2d2a
Chao Ma <machaosydney@gmail.com>
2023-07-12 13:57:50 +1000

    feat(express): Add Express app and server confi

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use(
+	cors({
+		credentials: true,
+	})
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(compression());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+	console.log('server is running on http://localhost:8080/');
+});
